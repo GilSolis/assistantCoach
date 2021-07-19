6d8d67aa83756139c73089e8454efce2ca4c0c7c
Gil Solis <gilsolisuw@gmail.com>
2021-07-18 20:58:24 -0700

    proxy seerver testing

--- public/js/getTeams.js
+++ public/js/getTeams.js
@@ -4,10 +4,19 @@ document.getElementById('getTeams').addEventListener('click', getTeams)
 async function getTeams(){
 
     let franchiseName = document.getElementById('franchiseName').value
-    const res = await fetch('https://api.myfantasyleague.com/2021/export?TYPE=myleagues&YEAR=2021&FRANCHISE_NAMES=' + `${franchiseName}`)
-    const results = await res.json()
-    console.log(results)
-
+    //test URLs
+    const baseURL = `https://proxygil1.herokuapp.com`;
+    const url = `${baseURL}/fetch`
+    fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        method: 'get', // I assume you're using get requests to communicate with the myfantasyleague api, but if not you can change this to post or whatever
+        searchURL: 'https://pokeapi.co/api/v2/pokemon/ditto' // here you put the full url to myfantasyleague.com including query parameters and everything. change this parameter whenever you need to make a request for different info from myfantasyleague
+    })
+    }).then(res=> res.json()).then(console.log);
 }
-getTeams()
+
 
